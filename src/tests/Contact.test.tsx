e20798d54b031a7dd0bb04b303171080b0773036
danendradipa <danendrach@gmail.com>
2024-12-01 14:24:47 +0700

    modify contact test component

--- src/tests/Contact.test.tsx
+++ src/tests/Contact.test.tsx
@@ -13,26 +13,22 @@ describe("Contact Component", () => {
   });
 
   test("form submission works", () => {
-    // Render komponen Contact
+
     render(<Contact />);
 
-    // Menemukan elemen input dan tombol
     const fullNameInput = screen.getByLabelText(/Full Name/i);
     const emailInput = screen.getByLabelText(/Email/i);
     const messageInput = screen.getByLabelText(/Message/i);
     const submitButton = screen.getByRole("button", { name: /Send Message/i });
 
-    // Simulasi input pada form
     fireEvent.change(fullNameInput, { target: { value: "John Doe" } });
     fireEvent.change(emailInput, { target: { value: "john@example.com" } });
     fireEvent.change(messageInput, { target: { value: "Hello, I would like to collaborate!" } });
 
-    // Memeriksa apakah nilai input sudah berubah
     expect(fullNameInput).toHaveValue("John Doe");
     expect(emailInput).toHaveValue("john@example.com");
     expect(messageInput).toHaveValue("Hello, I would like to collaborate!");
 
-    // Simulasi klik tombol kirim
     fireEvent.click(submitButton);
 
     expect(screen.getByText(/Send Message/i)).toBeInTheDocument();
