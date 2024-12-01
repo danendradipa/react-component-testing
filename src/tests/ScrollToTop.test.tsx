9949a98cf98e1beda2ee95e8d28e951928e24c4b
danendradipa <danendrach@gmail.com>
2024-12-01 14:25:01 +0700

    modify scroll to top test component

--- src/tests/ScrollToTop.test.tsx
+++ src/tests/ScrollToTop.test.tsx
@@ -1,6 +1,6 @@
 // ScrollToTop.test.tsx
 import { render } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom"; // Untuk merender komponen dalam konteks router
+import { MemoryRouter } from "react-router-dom"; 
 import ScrollToTop from "../component/ScrollToTop";
 
 describe("ScrollToTop component", () => {
@@ -9,17 +9,14 @@ describe("ScrollToTop component", () => {
     const scrollToMock = jest.fn();
     window.scrollTo = scrollToMock;
 
-    // Render ScrollToTop dalam konteks MemoryRouter
     render(
       <MemoryRouter initialEntries={['/initial']}>
         <ScrollToTop />
       </MemoryRouter>
     );
 
-    // Simulasi perubahan pathname
     window.history.pushState({}, '', '/new-path');
 
-    // Memastikan bahwa window.scrollTo dipanggil setelah pathname berubah
     expect(scrollToMock).toHaveBeenCalledWith(0, 0);
   });
 });
